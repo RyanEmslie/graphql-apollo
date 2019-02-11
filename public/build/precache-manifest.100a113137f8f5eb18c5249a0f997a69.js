6f4a057b4f3b6b1c4c21c39bdaeeb4de766ee0e7
RyanEmslie <ryan.emslie@gmail.com>
2019-02-10 21:37:43 -0500

    new build attempt

---
+++ public/build/precache-manifest.100a113137f8f5eb18c5249a0f997a69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "01fca996f8ed1df9edf5cba60d163543",
+    "url": "/static/media/roadster.01fca996.png"
+  },
+  {
+    "revision": "2f8bbf8f2484dcd205f85cf18cacbe05",
+    "url": "/static/media/logo.2f8bbf8f.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e4db78d9fdcaf45e49aa",
+    "url": "/static/js/main.e4db78d9.chunk.js"
+  },
+  {
+    "revision": "ef5d66b6c633ec61abd8",
+    "url": "/static/js/1.ef5d66b6.chunk.js"
+  },
+  {
+    "revision": "e4db78d9fdcaf45e49aa",
+    "url": "/static/css/main.f6fd45d8.chunk.css"
+  },
+  {
+    "revision": "c101247ad242418da558ffc4c3f2c60a",
+    "url": "/index.html"
+  }
+];
